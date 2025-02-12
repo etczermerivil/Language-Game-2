60ecf6f6e721932130a184c348c9c2dd383fb68e
Etczer <etczermerivil@gmail.com>
2025-02-12 09:17:36 -0500

    updated

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,15 @@
+# backend/config.py
 import os
 
+# If you want to switch logic based on FLASK_ENV:
+environment = os.getenv('FLASK_ENV', 'development')
+
+# If you use a Postgres schema in production:
+SCHEMA = 'language_game'  # Or whatever your schema name is
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
-    # (only 'postgresql') but heroku's postgres add-on automatically sets the
-    # url in the hidden config vars to start with postgres.
-    # so the connection uri must be updated here (for production)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
