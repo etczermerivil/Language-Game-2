c2fef8a98c98320509c6d2d96fac20e0fa119c19
Etczer <etczermerivil@gmail.com>
2025-02-12 11:42:49 -0500

    fixed revision error hopefully

---
+++ migrations/versions/f9dfbd8d0075_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f9dfbd8d0075
+Revises: 
+Create Date: 2025-02-12 11:39:57.172523
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f9dfbd8d0075'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
