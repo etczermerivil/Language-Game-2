857e66e77d8f5178518257dc6bad342043d2f668
Etczer <etczermerivil@gmail.com>
2025-02-12 01:30:08 -0500

    updated entrypoitnz'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e  # Exit immediately if a command fails
+export FLASK_APP=backend
 
 # Run migrations
 flask db upgrade
+
 
 # Run seeding
 flask seed all || echo "Seeding failed or was already applied."
